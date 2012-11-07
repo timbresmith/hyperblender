timbresmith/hyperblender

commit 7483ae32da84d08db53ea34bbfceae366e9033fd

Fix [#31977] Export Animated Mesh, terminate baking in fluid-sim
* Elbeem exporter code now overrides user settings to No Slip in case the object is animated;
* UI of fluid obstacles now disables slip settings when export animated is enabled;
* Added in this later option's tooltip a mention that it enforces No Slip!

git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk/blender@48531 954f8c5b-7b00-dc11-b283-0030488c597c

source/blender/editors/physics/physics_fluid.c
@@ -567,11 +567,14 @@ static void export_fluid_objects(ListBase *fobjects, Scene *scene, int length)
 		if (deform) {
 			fsmesh.channelSizeVertices = length;
 			fsmesh.channelVertices = fobj->VertexCache;
-				
-			// remove channels
+			
+			/* remove channels */
 			fsmesh.channelTranslation      = 
 			fsmesh.channelRotation         = 
-			fsmesh.channelScale            = NULL; 
+			fsmesh.channelScale            = NULL;
+			
+			/* Override user settings, only noslip is supported here! */
+			fsmesh.obstacleType = FLUIDSIM_OBSTACLE_NOSLIP;
 		}
 		
 		elbeemAddMesh(&fsmesh);

release/scripts/startup/bl_ui/properties_physics_fluid.py
@@ -117,10 +117,14 @@ class PHYSICS_PT_fluid(PhysicButtonsPanel, Panel):
             col.prop(fluid, "use_animated_mesh")
 
             col = split.column()
-            col.label(text="Slip Type:")
-            col.prop(fluid, "slip_type", text="")
+            subsplit = col.split()
+            subcol = subsplit.column()
+            if fluid.use_animated_mesh:
+                subcol.enabled = False
+            subcol.label(text="Slip Type:")
+            subcol.prop(fluid, "slip_type", text="")
             if fluid.slip_type == 'PARTIALSLIP':
-                col.prop(fluid, "partial_slip_factor", slider=True, text="Amount")
+                subcol.prop(fluid, "partial_slip_factor", slider=True, text="Amount")
 
             col.label(text="Impact:")
             col.prop(fluid, "impact_factor", text="Factor")

source/blender/makesrna/intern/rna_fluidsim.c
@@ -461,8 +461,9 @@ static void rna_def_fluidsim_volume(StructRNA *srna)
 	RNA_def_property_boolean_sdna(prop, NULL, "domainNovecgen", 0);
 	RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
 	RNA_def_property_ui_text(prop, "Export Animated Mesh",
-	                         "Export this mesh as an animated one (slower, only use if really necessary [e.g. "
-	                         "armatures or parented objects], animated pos/rot/scale F-Curves do not require it)");
+	                         "Export this mesh as an animated one (slower and enforces No Splip, only use if really "
+	                         "necessary [e.g. armatures or parented objects], animated pos/rot/scale F-Curves "
+	                         "do not require it)");
 }
 
 static void rna_def_fluidsim_active(StructRNA *srna)

commit 105352951b1b17e54fd523913309bb1134d9110e

Bugfix [#32939] Fluid sim reversed on X and Y with animated Control object
Bug was introduced in r48531 by accident since fluid control object abuse a variable for another flag which got reset.

git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk/blender@51985 954f8c5b-7b00-dc11-b283-0030488c597c

source/blender/editors/physics/physics_fluid.c
@@ -574,7 +574,8 @@ static void export_fluid_objects(ListBase *fobjects, Scene *scene, int length)
 			fsmesh.channelScale            = NULL;
 			
 			/* Override user settings, only noslip is supported here! */
-			fsmesh.obstacleType = FLUIDSIM_OBSTACLE_NOSLIP;
+			if (fsmesh.type != OB_FLUIDSIM_CONTROL)
+				fsmesh.obstacleType = FLUIDSIM_OBSTACLE_NOSLIP;
 		}
 		
 		elbeemAddMesh(&fsmesh);