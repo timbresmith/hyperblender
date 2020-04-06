c3404fe61be3c79d7b2a3edaac19dd9907436191
Micah Fitch <fitchmicah@gmail.com>
2020-04-06 13:29:30 -0500

    got some GAL code compiling!

--- source/blender/modifiers/intern/MOD_moebius.cpp
+++ source/blender/modifiers/intern/MOD_moebius.cpp
@@ -1,10 +1,22 @@
 #include "BKE_modifier.h"
 #include "DNA_mesh_types.h"
+#include "gal/pga.hpp"
+
+using namespace gal::pga;
+using gal::compute;
 
 static Mesh *moebius_applyModifier(struct ModifierData *md,
                                    const struct ModifierEvalContext *ctx,
                                    struct Mesh *mesh)
 {
+  point<> p1{1, 0, 0}; // The default value type is `float`
+  point<> p2{0, 1, 1};
+
+  // Take the regressive product of the two points to construct
+  // the line passing through them.
+  line<> l = compute([](auto p1, auto p2) {
+          return p1 & p2;
+      }, p1, p2);
   return mesh;
 }
 
@@ -13,10 +25,12 @@ ModifierTypeInfo modifierType_Moebius = {
     /* structName */ "MoebiusModifierData",
     /* structSize */ sizeof(MoebiusModifierData),
     /* type */ eModifierTypeType_DeformOrConstruct,
-    /* flags */ 0
-      | eModifierTypeFlag_AcceptsMesh
+    /* flags */ 
+    ModifierTypeFlag(
+      eModifierTypeFlag_AcceptsMesh
       | eModifierTypeFlag_SupportsEditmode
-      | eModifierTypeFlag_AcceptsCVs,
+      | eModifierTypeFlag_AcceptsCVs
+    ) ,
     /* copyData */ NULL,
 
     /* deformVerts */ NULL,
